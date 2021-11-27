a1d28a1ca9a93d1abf9e3b71e6f016919212464e
wolf811 <wolf811@gmail.com>
2021-11-27 19:52:29 +0300

    les06 end

--- vk_les06_hw.sql
+++ vk_les06_hw.sql
@@ -4,21 +4,38 @@ USE vk;
 /*1. Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, 
 	который больше всех общался с нашим пользователем.*/
 
--- SELECT 
--- 	MSG.to_user_id AS friend, COUNT(*) AS count_messages, MSG.from_user_id AS my_user, FR.status
--- 	FROM messages MSG LEFT JOIN friend_requests FR ON (FR.initiator_user_id=MSG.from_user_id AND MSG.to_user_id=FR.target_user_id )
--- 	WHERE MSG.from_user_id = 1 AND FR.status='approved'
--- 	GROUP BY friend ORDER BY count_messages DESC LIMIT 1;
+-- этот скрипт работает, но только мы еще это не проходили (подсказали)
 SELECT 
-	MSG.from_user_id AS my_user, MSG.to_user_id AS friend, FR.status, COUNT(*) AS count_messages
-	FROM messages AS MSG, friend_requests AS FR
-	WHERE FR.initiator_user_id = 1 AND FR.status='approved' AND MSG.from_user_id = 1
-	GROUP BY friend ORDER BY count_messages DESC LIMIT 1;
+	MSG.to_user_id AS friend, COUNT(*) AS count_messages, MSG.from_user_id AS my_user, FR.status
+	FROM messages MSG LEFT JOIN friend_requests FR ON (FR.initiator_user_id=MSG.from_user_id AND MSG.to_user_id=FR.target_user_id )
+	WHERE MSG.from_user_id = 1 AND FR.status='approved'
+	GROUP BY friend ORDER BY count_messages DESC;
 
--- что-то неправильно
+-- не получается правильно вставить вложенный запрос
+SELECT 
+	from_user_id AS my_user,
+	to_user_id AS friend,
+	COUNT(to_user_id) AS 'count_messages'
+FROM messages WHERE from_user_id = 1 AND to_user_id IN (SELECT status FROM friend_requests WHERE status = 'approved')
+GROUP BY to_user_id ORDER BY count_messages DESC LIMIT 1;
+
+SELECT status FROM friend_requests WHERE status = 'approved';
 
 -- 2. Подсчитать общее количество лайков, которые получили пользователи младше 10 лет.
 
+-- общее кол-во лайков всех пользователей
+SELECT COUNT(*) AS 'count_likes' FROM likes;
+-- пользователи младше 10 лет
+SELECT user_id FROM profiles WHERE birthday > (CURRENT_DATE - INTERVAL 10 YEAR);
 
+-- итоговый запрос
+SELECT COUNT(*) AS 'count_likes' FROM likes WHERE user_id IN 
+(SELECT user_id FROM profiles WHERE birthday > (CURRENT_DATE - INTERVAL 10 YEAR));
 
 -- 3. Определить кто больше поставил лайков (всего): мужчины или женщины.
+-- ничего умней придумать не удалось
+SELECT 
+	user_id,
+	(SELECT gender FROM profiles WHERE user_id = likes.user_id) AS gender,
+	COUNT(*)
+FROM likes GROUP BY user_id ORDER BY gender;
